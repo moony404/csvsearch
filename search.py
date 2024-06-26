d9269c99e5becc823a2b163b0a23d5400b105494
moony404 <theo.marliac63@gmail.com>
2024-06-26 20:05:04 +0200

    colors

--- search.py
+++ search.py
@@ -1,7 +1,7 @@
 import csv
 import pandas as pd
 from colorama import Fore
-def chercher_mot_dans_csv(file_name, mot):
+def chercher_mot_dans_csv(file_name, search_keyword):
     try:
         with open(file_name, mode='r', encoding='utf-8') as file:
             reader = csv.reader(file)
@@ -9,17 +9,18 @@ def chercher_mot_dans_csv(file_name, mot):
             result = []
             for row in reader:
                 for cell in row:
-                    if mot in cell:
+                    if search_keyword in cell:
                         print(f"{Fore.YELLOW}")
-                        print(f"{Fore.BLUE}found one instance of '{Fore.CYAN}{mot}{Fore.BLUE}' in => \n{Fore.WHITE}{row}")
+                        print(f"{Fore.BLUE}found one instance of '{Fore.CYAN}{search_keyword}{Fore.BLUE}' in => \n{Fore.WHITE}{row}")
                         print(f"{Fore.WHITE}", end='')
                         print(f"{Fore.YELLOW}", end='')
                         print("row count : ", end='')
                         print(row.count)
                         print(f"{Fore.WHITE}")
-        return f"Mot '{mot}' non trouvé dans le fichier."
+        return f"Mot '{search_keyword}' non trouvé dans le fichier."
+        print(f"{Fore.BLUE}[notice] {Fore.YELLOW}The word {Fore.WHITE}'{search_keyword}'{Fore.YELLOW} wasn't found.{Fore.WHITE}")
     except FileNotFoundError:
-        print(f"{Fore.BLUE}[{Fore.RED}error{Fore.BLUE}] {Fore.YELLOW}The file {Fore.WHITE}'{file_name}'{Fore.YELLOW} was not found.{Fore.WHITE}")
+        print(f"{Fore.BLUE}[{Fore.RED}error{Fore.BLUE}] {Fore.YELLOW}The file {Fore.WHITE}'{file_name}'{Fore.YELLOW} wasn't found.{Fore.WHITE}")
 # settings
 file_name = 'samples\\LGE_File config.csv'
 search_keyword = '\\'
